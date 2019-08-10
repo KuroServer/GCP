5593b842505a9d53b263968aea732b69eb1502bc
KuroServer <Administrator@kuroserver.net>
2019-08-10 10:52:25 +0900

    Update docker-entrypoint.sh

--- Dockerfile/docker-entrypoint.sh
+++ Dockerfile/docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+GCS_BUCKET_NAME=thinker-school
+
 # env check
-if [ -z $GCS_BUCKET_NAME ]; then
-    echo "Empty GCS_BUCKET_NAME..." >&2
-    exit 1
-fi
+#if [ -z $GCS_BUCKET_NAME ]; then
+#    echo "Empty GCS_BUCKET_NAME..." >&2
+#    exit 1
+#fi
 
 # make mount point
 mkdir -p /mnt/gcs/$GCS_BUCKET_NAME
@@ -13,15 +15,15 @@ mkdir -p /mnt/gcs/$GCS_BUCKET_NAME
 gcsfuse -o allow_other $GCS_BUCKET_NAME /mnt/gcs/$GCS_BUCKET_NAME
 
 # create symlink
-if [ -L /var/lib/nginx/html ]; then
-    rmdir /var/lib/nginx/html
+if [ -L /var/www/html ]; then
+    rmdir /var/www/html
 else
-    rm -rf /var/lib/nginx/html
+    rm -rf /var/www/html
 fi
-ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /var/lib/nginx/html
+ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /var/www/html
 
 # main process
-nginx -g "daemon off;"
+httpd -g "daemon off;"
 
 # unmount
 fusermount -u /mnt/gcs/$GCS_BUCKET_NAME
