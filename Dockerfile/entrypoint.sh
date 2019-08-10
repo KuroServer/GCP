abf4e05ae6ad86dd1911c5262141db3254aa8641
KuroServer <Administrator@kuroserver.net>
2019-08-10 13:02:40 +0900

    Update entrypoint.sh

--- Dockerfile/entrypoint.sh
+++ Dockerfile/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /etc/apache2/envvars
 chown -R www-data:www-data *
-sudo -u www-data gcsfuse example-bucket ./
+sudo -u www-data gcsfuse thinker-school ./
 exec apache2 -D FOREGROUND
