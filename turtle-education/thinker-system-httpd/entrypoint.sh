720931a8e24e64071bcf36b441daa55d6845e7b2
KuroServer <Administrator@kuroserver.net>
2019-08-12 10:59:11 +0900

    Update entrypoint.sh

--- turtle-education/thinker-system-httpd/entrypoint.sh
+++ turtle-education/thinker-system-httpd/entrypoint.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
 
+mkdir /var/run/apache2
+
+#Set GCS_BUCKET_NAME
+GCS_BUCKET_NAME="thinker-system"
+
+# env check
+if [ -z $GCS_BUCKET_NAME ]; then
+    echo "Empty GCS_BUCKET_NAME..." >&2
+    exit 1
+fi
+
+# make mount point
+mkdir -p /mnt/gcs/$GCS_BUCKET_NAME
+
+# mount gcs
+sudo gcsfuse -o allow_other $GCS_BUCKET_NAME /mnt/gcs/$GCS_BUCKET_NAME
+
+# create symlink
+if [ -L /var/www/html ]; then
+    rmdir /var/www/html
+else
+    rm -rf /var/www/html
+fi
+ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /var/www/html
+
+exec apache2 -D FOREGROUND
