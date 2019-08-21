9027126c5f3267e78f75145bb03b814b0728a829
KuroServer <Administrator@kuroserver.net>
2019-08-22 06:05:10 +0900

    Update entrypoint-manage.sh

--- turtle-education/thinker-manage-httpd/entrypoint-manage.sh
+++ turtle-education/thinker-manage-httpd/entrypoint-manage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/sbin/init
+exec /usr/sbin/init
 sudo systemctl start rpcbind 
 mkdir -p /mnt/disks/thinker-disk
 sudo mount -t nfs 10.146.15.198:/mnt/disks/thinker-disk /mnt/disks/thinker-disk
