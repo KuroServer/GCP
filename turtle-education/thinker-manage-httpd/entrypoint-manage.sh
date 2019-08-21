09d461578cf622893212ae969e4dc3bc12c9a08e
KuroServer <Administrator@kuroserver.net>
2019-08-21 18:57:54 +0900

    Update entrypoint-manage.sh

--- turtle-education/thinker-manage-httpd/entrypoint-manage.sh
+++ turtle-education/thinker-manage-httpd/entrypoint-manage.sh
@@ -2,6 +2,6 @@
 
 mkdir -p /mnt/disks/thinker-disk
 
-mount -t nfs 10.146.15.198:/mnt/disks/thinker-disk /mnt/disks/thinker-disk
+#mount -t nfs 10.146.15.198:/mnt/disks/thinker-disk /mnt/disks/thinker-disk
 
 exec /usr/sbin/httpd -D FOREGROUND
