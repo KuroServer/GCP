633eee77231b5cd6ea7fbe7f4c109a4cd8d5c635
KuroServer <Administrator@kuroserver.net>
2019-08-21 15:30:59 +0900

    Update entrypoint-manage.sh

--- turtle-education/thinker-manage-httpd/entrypoint-manage.sh
+++ turtle-education/thinker-manage-httpd/entrypoint-manage.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-mkdir /var/run/apache2
-
-#Set GCS_BUCKET_NAME
-GCS_BUCKET_NAME="thinker-manage"
-
-# env check
-if [ -z $GCS_BUCKET_NAME ]; then
-    echo "Empty GCS_BUCKET_NAME..." >&2
-    exit 1
-fi
-
-# make mount point
-mkdir -p /mnt/gcs/$GCS_BUCKET_NAME
-
-# mount gcs
-sudo gcsfuse --implicit-dirs -o allow_other $GCS_BUCKET_NAME /mnt/gcs/$GCS_BUCKET_NAME
-
-# create symlink
-if [ -L /var/www/html ]; then
-    rmdir /var/www/html
-else
-    rm -rf /var/www/html
-fi
-ln -sfn /mnt/gcs/$GCS_BUCKET_NAME /var/www/html
+mkdir /mnt/disks/thinker
 
 exec apache2 -D FOREGROUND
