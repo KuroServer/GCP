54ef8d8ae609f5241991d9a1944d3d2d89fc7258
KuroServer <Administrator@kuroserver.net>
2019-08-22 03:47:35 +0900

    Update entrypoint-manage.sh

--- turtle-education/thinker-manage-httpd/entrypoint-manage.sh
+++ turtle-education/thinker-manage-httpd/entrypoint-manage.sh
@@ -2,6 +2,9 @@
 
 mkdir -p /mnt/disks/thinker-disk
 
+systemctl start rpcbind 
+ ystemctl enable rpcbind 
+
 #mount -t nfs 10.146.15.198:/mnt/disks/thinker-disk /mnt/disks/thinker-disk
 
 exec /usr/sbin/httpd -D FOREGROUND
